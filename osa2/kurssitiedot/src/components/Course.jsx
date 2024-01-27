74cb13f38f1df310d92898a7bb5f0e5a1dea24c5
sanku <sanna.kukkonen2@metropolia.fi>
2024-01-27 14:56:06 +0200

    2.2-2.3: kurssitiedot step7-8

--- osa2/kurssitiedot/src/components/Course.jsx
+++ osa2/kurssitiedot/src/components/Course.jsx
@@ -3,6 +3,7 @@ const Course = ({course}) => {
         <>
             <Header text={course.name}/>
             <Content course={course.parts}/>
+            <Total course={course.parts}/>
         </>
     );
 };
@@ -13,13 +14,24 @@ const Content = ({course}) => {
     return(
     <>
         {course.map(course => 
-            <Part key={course.id} 
+          <Part key={course.id} 
             name={course.name} 
             ex={course.exercises}/>)
         }
      </> 
     )
 }
+
+const Total = ({course}) => {
+    const count = course.map(course => course.exercises)
+    return(
+        <p>
+            <b>total of {count.reduce((total, count) => count + total, 0)} exercises</b>
+        </p>
+    )
+
+}
+
 
 const Part = ({name, ex}) => <p>{name} {ex}</p>
 
